cff18114e69b776fcd57c997201852d94660135b
Vamshireddy495 <59283825+Vamshireddy495@users.noreply.github.com>
2026-02-02 09:45:05 -0500

    Initial commit

--- import_data.py
+++ import_data.py
@@ -8,10 +8,4 @@ df = kagglehub.load_dataset(
   KaggleDatasetAdapter.PANDAS,
   "nehalbirla/vehicle-dataset-from-cardekho",
   file_path
-  # Provide any additional arguments like 
-  # sql_query or pandas_kwargs. See the 
-  # documenation for more information:
-  # https://github.com/Kaggle/kagglehub/blob/main/README.md#kaggledatasetadapterpandas
 )
-
-print("First 5 records:", df.head())
